6eebd2b59670e49e0b66b56cc94c44168895eb7c
Adona12 <adonatesfaye99@gmail.com>
2023-04-18 14:13:32 +0300

    Make header and footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,13 +12,13 @@ import {
 function Footer() {
   return (
     <>
-      <div className="grid md:grid-cols-6 mx-6 justify-center md-justify-start">
+      <div className="grid md:grid-cols-6 mx-6 justify-center md:justify-start">
         <div className="self-center">
           <img src="footer.png" alt="" />
         </div>
         <div className="md:justify-self-center pt-6 md:col-span-2">
           <div className="block font-bold mb-5 text-sm ">
-            Get involved in improving tech <br/>
+            Get involved in improving tech <br />
             education in africa
           </div>
           <div className="btn px-12 py-3 text-sm mb-4 md:m-0">Support US</div>
@@ -63,13 +63,16 @@ function Footer() {
       </div>
       <div className="h-px  my-6  bg-gray-100"></div>
       <div className="md:flex justify-between ">
-        <div className="flex items-baseline"><FaCopyright className="text-gray-400 mr-2"/>2020 Africa to Silicon Valley, Inc. All right reserved.</div>
+        <div className="flex items-baseline text-sm">
+          <FaCopyright className="text-gray-400 mr-2" />
+          2020 Africa to Silicon Valley, Inc. All right reserved.
+        </div>
         <div className="flex ">
           <FaTwitter className="ml-8 text-gray-400" />
-          <FaFacebookF className="ml-8 text-gray-400"/>
-          <FaYoutube className="ml-8 text-gray-400"/>
-          <FaLinkedin className="ml-8 text-gray-400"/>
-          <FaInstagram className="ml-8 text-gray-400"/>
+          <FaFacebookF className="ml-8 text-gray-400" />
+          <FaYoutube className="ml-8 text-gray-400" />
+          <FaLinkedin className="ml-8 text-gray-400" />
+          <FaInstagram className="ml-8 text-gray-400" />
         </div>
       </div>
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,71 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+
 function Header() {
+  const [nav, setNav] = useState(false);
   return (
-    <div className="flex justify-between  pt-6">
-      {/* <Image src="logo.png" alt="" width="2" height="3"></Image> */}
-      <img src="logo.png" />
-      <ul className="flex">
-        <li>
-          <Link href="/" className="nav">
-            Home
+    <div className="relative">
+      <div className=" sm:flex justify-between items-center pt-6">
+        {/* <Image src="logo.png" alt="" width="2" height="3"></Image> */}
+        <img src="logo.png" />
+        <ul className={`sm:flex  mt-6 mb-2 md:mt-0 md:mb-2 items-baseline ${nav ? "block" : "hidden"}`}>
+          <li>
+            <Link href="/" className="nav \">
+              Home
+            </Link>
+          </li>
+          <Link href="/teams" className="nav">
+            Teams
           </Link>
-        </li>
-        <Link href="/teams" className="nav">
-          Teams
-        </Link>
-        <li>
-          <Link href="/success" className="nav">
-            Success Stories
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" className="nav">
-            About Us
-          </Link>
-        </li>
-        <li>
-          <Link href="/involved" className="nav">
-            Get Involved
-          </Link>
-        </li>
-      </ul>
-      <div>
-        <span className="font-bold text-sm">Login</span>
-        <div className="btn ml-4 px-3 py-1">Donate</div>
+          <li>
+            <Link href="/success" className="nav">
+              Success Stories
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="nav">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link href="/involved" className="nav">
+              Get Involved
+            </Link>
+          </li>
+        </ul>
+        <div className={`sm:inline-block items-baseline ${nav ? "block" : "hidden"}`}>
+          <span className="font-bold text-sm">Login</span>
+          <div className="btn ml-4 px-3 py-1">Donate</div>
+        </div>
       </div>
-    </div>
+      <div className="absolute right-2 top-7 sm:hidden">
+        <button
+          type="button"
+          className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          aria-controls="mobile-menu"
+          aria-expanded="false"
+          onClick={() => setNav(!nav)}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="block h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+      </div>
+    </ div>
   );
 }
 
